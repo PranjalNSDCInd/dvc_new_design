8abafb28def957f722e6051dcba01903c3851fb7
Pranjal Prasun <pranjal.prasun@nsdcindia.org>
2023-04-11 13:31:24 +0530

    change in otp view

--- src/app/modules/get-started/get-started.component.ts
+++ src/app/modules/get-started/get-started.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-get-started',
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
 })
 export class GetStartedComponent {
   openModal = false;
-
+  @ViewChild('otp2') otp2!: ElementRef;
+  @ViewChild('otp3') otp3!: ElementRef;
+  @ViewChild('otp4') otp4!: ElementRef;
+  @ViewChild('otp5') otp5!: ElementRef;
+  @ViewChild('otp6') otp6!: ElementRef;
   constructor(){}
 
   ngOnInit(){}
@@ -15,5 +19,21 @@ export class GetStartedComponent {
   changeModal(){
     debugger
     this.openModal = !this.openModal;
+  }
+
+
+  changeFocus(id: number){
+    debugger
+    if(id===1){
+      this.otp2.nativeElement.focus();
+    } else if (id===2){
+      this.otp3.nativeElement.focus();
+    }else if (id===3){
+      this.otp4.nativeElement.focus();
+    }else if (id===4){
+      this.otp5.nativeElement.focus();
+    }else if (id===5){
+      this.otp6.nativeElement.focus();
+    }
   }
 }
